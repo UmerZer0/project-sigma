334c583b582334b98448463d4045040918d3afa7
Rao Umar <uno4umer@gmail.com>
2025-02-02 14:28:36 +0500

    Changes to be committed: 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/Dashboard.jsx 	renamed:    frontend/src/pages/Invoice/Home.jsx -> frontend/src/components/invoice/Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
+import Dashboard from "./components/Dashboard";
+import Invoice from "./components/invoice/Home";
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <button>Stock</button>
-      <button>Invoice</button>
-      <button>Admin</button>
-      <button>Khata</button>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/invoice" element={<Invoice />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button>Stock</button>
+      <button>Invoice</button>
+      <button>Admin</button>
+      <button>Khata</button>
+    </>
+  )
+}
+
+export default Dashboard
