3df6c616043bb94ca6b2f3a6a5cc948c1d660c8b
Rao Umar <uno4umer@gmail.com>
2025-02-02 10:45:46 +0500

    On branch main Changes to be committed: 	modified:   electron/index.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	new file:   frontend/src/pages/Invoice/Home.jsx

--- electron/index.js
+++ electron/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 400,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
