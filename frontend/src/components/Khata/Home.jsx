3d496674b35260af877fd86232224b667015b695
Rao Umar <uno4umer@gmail.com>
2025-02-10 07:34:49 +0500

    Added markdown and styling for Khata

--- frontend/src/components/Khata/Home.jsx
+++ frontend/src/components/Khata/Home.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Link } from "react-router-dom"
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return(
-        <>
-            <div>This is Khata Home</div>
-            <Link to='/'> 
-            <button>
-                Back
-            </button>
-            </Link>
-        </>
-    )
+  return (
+    <>
+      <div className="back-btn-box">
+        <Link className="back-btn btn" to="/">
+          Back to Dashboard
+        </Link>
+      </div>
 
+      <div className="khata-body">
+        <section className="sale-section">
+          <h1>Sale</h1>
+        </section>
+        <section className="expense-section">
+          <h1>Expense</h1>
+          {/* <div className="expense-body">Hello</div> */}
+        </section>
+        <button className="add-btn btn">+</button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
