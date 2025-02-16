c9508bf9c7c639ea39f75f4208c62a9b8e586d6e
Rao Umar <uno4umer@gmail.com>
2025-02-16 19:12:57 +0500

    Refactor Khata component layout and add table structure for Sale and Expense sections

--- frontend/src/components/Khata/Home.jsx
+++ frontend/src/components/Khata/Home.jsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Back_Button from "../utilities/Back_Button";
 
 function Home() {
   return (
     <>
-      <Back_Button />
-
-      <div className="khata-body">
-        <section className="sale-section">
-          <h1>Sale</h1>
-        </section>
-        <section className="expense-section">
-          <h1>Expense</h1>
-          {/* <div className="expense-body">Hello</div> */}
-        </section>
-        <button className="add-btn btn">+</button>
+      <div className="outer-body">
+        <Back_Button />
+        <div className="khata-body">
+          <section className="sale-section">
+            <h1>Sale</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th className="first-col ">&nbsp;&nbsp;Name</th>
+                  <th>&nbsp;Amount</th>
+                </tr>
+              </thead>
+            </table>
+            <button className="add-btn btn">+</button>
+          </section>
+          <section className="expense-section">
+            <h1>Expense</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th className="first-col ">&nbsp;&nbsp;Name</th>
+                  <th>&nbsp;Type</th>
+                  <th>&nbsp;Amount</th>
+                </tr>
+              </thead>
+            </table>
+            <button className="add-btn btn">+</button>
+          </section>
+        </div>
       </div>
     </>
   );
